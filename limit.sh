92b599121b997b34bdfe41bad4018bc00dc8297e
x373241884y <x373241884y@gmail.com>
2016-10-15 10:53:08 +0800

    tools

---
+++ limit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+PIDS=`top -bn 1 | grep "^ *[1-9]" | awk '{ if($9 > 30 || $10 > 10 && id -u $2 > 500) print $1}'`
+for PID in $PIDS
+do
+        renice +10 $PID
+        echo "renice +10 $PID"
+done
