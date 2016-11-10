52e48922c16aa41b3d6a9cd2d49ad9fccd567ccd
x373241884y <x373241884y@gmail.com>
2016-11-10 17:14:22 +0800

    set log

--- limit.sh
+++ limit.sh
@@ -3,5 +3,5 @@ PIDS=`top -bn 1 | grep "^ *[1-9]" | awk '{ if($9 > 30 || $10 > 10 && id -u $2 > 
 for PID in $PIDS
 do
         renice +10 $PID
-        echo "renice +10 $PID"
+        echo "renice +10 $PID">>/tmp/limit.log
 done
